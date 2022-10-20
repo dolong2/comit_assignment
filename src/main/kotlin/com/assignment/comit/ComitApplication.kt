ec8a1c68732e02cfa89915f3c0bc50bf077977ee
jeayoung <k01066624566@gmail.com>
2022-10-20 11:13:13 +0900

    init setting

---
+++ src/main/kotlin/com/assignment/comit/ComitApplication.kt
@@ -0,0 +1,11 @@
+package com.assignment.comit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComitApplication
+
+fun main(args: Array<String>) {
+	runApplication<ComitApplication>(*args)
+}
---
+++ src/test/kotlin/com/assignment/comit/ComitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.assignment.comit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
