14b1b8ff24df466592e7fd313e1529b475fc5b21
jeayoung <k01066624566@gmail.com>
2022-10-25 10:49:54 +0900

    add cors config

---
+++ src/main/kotlin/com/assignment/comit/global/configuration/web/WebConfig.kt
@@ -0,0 +1,23 @@
+package com.assignment.comit.global.configuration.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    //CORS 설정
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.HEAD.name,
+                HttpMethod.POST.name,
+                HttpMethod.PUT.name,
+                HttpMethod.DELETE.name
+            )
+            .maxAge(3600)
+    }
+}
